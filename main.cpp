1df72c57fb86e2655664b9ebd8bd8fb4a7434416
cri-cov <cristian.covato@marconibari.edu.it>
2024-09-30 15:55:33 +0000

    consegna

--- main.cpp
+++ main.cpp
@@ -1,11 +1,35 @@
 #include <iostream>
+
 using namespace std;
 
 int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+  float a, b, c, max;
+  
+  cout << " inserisci 3 numeri " << endl;
+  cin >> a;
+  cin >> b;
+  cin >> c;
+  if (a > b) 
+  {
+    if (a > c) 
+    {
+      max = a;
+    } 
+    else 
+    {
+      max = c;
+    }
+  }
+  else 
+  {
+    if (b > c) 
+    {
+      max = b;
+    } 
+    else 
+    {
+      max = c;
+    }
+  }
+  cout << " il numero maggiore fra i tre numeri è " << max << endl;
 }
-
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
