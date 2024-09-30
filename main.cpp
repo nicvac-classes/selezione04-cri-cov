34b36e0ccbd8d10c565f26ccbd858107f467da93
cri-cov <cristian.covato@marconibari.edu.it>
2024-09-30 16:11:47 +0000

    consegna

--- main.cpp
+++ main.cpp
@@ -1,10 +1,8 @@
 #include <iostream>
 using namespace std;
-
 int main() {
   float a, b, c, max;
-  
-  cout << " inserisci 3 numeri " << endl;
+   cout << " inserisci 3 numeri " << endl;
   cin >> a;
   cin >> b;
   cin >> c;
